659f4850ca0f61d59cfffaf7baa16c9ed1dd3428
JulianSaucedo <julianelio89@outlook.es>
2023-11-16 18:59:12 -0300

    commit de  header

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get('/productDetail',(req,res) => res.sendFile(path.join(__dirname,'views','
 app.get('/productCart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')))
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
-
+app.get('/header',(req,res) => res.sendFile(path.join(__dirname,'views','partials','header.html')))
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
