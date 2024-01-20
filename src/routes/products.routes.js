398f29a86ecd122e35f710145980d99f0e7afde4
Nicole Colmenarez <nicolecolmenares412@gmail.com>
2024-01-19 22:58:33 -0300

    subo productView

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { detail, add, edit, remove, create } = require('../controllers/productsController');
 const upload = require('../middlewares/upload');
+const productView = require('../controllers/productsController/productView');
+
+
 
 
 const router = express.Router();
@@ -9,6 +12,7 @@ const router = express.Router();
 router.get('/detalle/:id?',detail );
 router.get('/agregar',add );
 router.get('/editar/:id?',edit );
+router.get('/listado', productView )
 router.post('/crear',upload.single('mainImage'),create)
 router.delete('/eliminar/:id', remove )
 
