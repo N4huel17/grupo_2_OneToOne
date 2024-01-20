417d2e74acfbc6a7c6551613f08cf2a088555a29
N4huel17 <jesusdsm17@gmail.com>
2024-01-20 13:32:00 -0300

    edit terminado

--- src/controllers/productsController/edit.js
+++ src/controllers/productsController/edit.js
@@ -1,21 +1,4 @@
 const { leerJSON } = require("../../data");
-<<<<<<< HEAD
-const categories = require('../../data/products.json')
-
-module.exports = (req,res) => {
-
-    const {id} = req.params;
-
-    const products = leerJSON('products');
-
-    const product = products.find(product => product.id == id);
-
-    return res.render('products/product-edit',{
-        ...product,
-        categories
-    })
-}
-=======
 
 module.exports= (req,res)=>{
     const {id} = req.params;
@@ -26,4 +9,3 @@ module.exports= (req,res)=>{
     })
 
 } 
->>>>>>> G2O-55-CRUD-editar-un-producto-formulario-de-edici-n-de-producto-y-l-gica-de-actualizaci-n.-incluyendo-edici-n-de-im-genes
