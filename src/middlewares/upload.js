38c71afce2cbe3c7be9ae8e3ef415c2d0d340423
N4huel17 <jesusdsm17@gmail.com>
2024-01-19 22:58:48 -0300

    .

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
         callback(null, 'public/img/product-img')
     },
     filename : (req ,file, callback) => {
-        callback(null,`${Date.now()}_products${path.extname(file.originalname)}`)
+        callback(null,`${Date.now()}_products_${path.extname(file.originalname)}`)
     }
 });
 
