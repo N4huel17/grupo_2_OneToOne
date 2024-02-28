dbe9fcfc93174c6f76e91fb560b2e132b0d92a5e
N4huel17 <jesusdsm17@gmail.com>
2024-02-28 00:13:57 -0300

    arreglos

---
+++ src/controllers/productsController/update.js
@@ -0,0 +1,149 @@
+const { validationResult } = require("express-validator");
+const { existsSync, unlinkSync } = require("fs");
+const db = require('../../database/models')
+
+module.exports = (req, res) => {
+
+
+
+    const image = req.files.image;
+    const images = req.files.images;
+
+
+    const {
+
+        name,
+        description,
+        category,
+        sizes,
+        colors,
+        price,
+        descount,
+        categoryId,
+        setionId,
+    } = req.body;
+
+    const { id } = req.params;
+
+    const errors = validationResult(req);
+
+
+    if (errors.isEmpty()) {
+        db.products.findByPk(id, {
+            include: ['images']
+        }).then((products) => {
+            if (image) {
+                existsSync("public/img/" + products.image) &&
+                    unlinkSync("public/img/" + products.image)
+            }
+            db.products.update({
+                name,
+                description,
+                category,
+                sizes,
+                colors,
+                price,
+                image: image ? image[0].filename : image,
+                descount,
+                categoryId,
+                setionId,
+
+            },
+                {
+                    where: {
+                        id,
+                    },
+                })
+                .then(() => {
+                    if (images) {
+                        products.images.forEach((image) => {
+                            existsSync("public/img/" + image.file) &&
+                                unlinkSync("public/img/" + image.file);
+                        });
+
+                        db.images.destroy({
+                            where: {
+                                productsId: id
+                            }
+                        }).then(() => {
+                            const imagesDB = images.map(images => {
+                                return {
+                                    file: images.filename,
+                                    restaurantId: products.id
+                                }
+                            })
+
+                            db.images.bulkCreate(imagesDB, {
+                                validate: true
+                            }).then(result => {
+                                console.log(result);
+                                return res.redirect("/admin");
+                            })
+                        })
+                    } 
+                });
+
+        })
+
+
+            .then(product => {
+                console.log(product);
+                this.images = images ? images.map(image => image.filename) : [];
+
+                if (images) {
+
+                    const imagesDB = images.map(image => {
+                        return {
+                            file: image.filename,
+                            productsId: product.id
+                        }
+                    })
+
+                    db.images.bulkCreate(imagesDB, {
+                        validate: true
+                    }).then(result => {
+                        console.log(result);
+                        return res.redirect("/admin");
+                    })
+                } else {
+                    return res.redirect("/admin");
+                }
+
+
+            }).catch(error => console.log(error))
+
+
+            
+    } else {
+        image &&
+          existsSync("public/img/" + image.filename) &&
+          unlinkSync("public/img/" + image.filename);
+    
+        if (images) {
+          images.forEach((image) => {
+            existsSync("public/img/" + image) &&
+              unlinkSync("public/img/" + image);
+          });
+        }
+      
+    
+        const products = db.products.findByPk(id, {
+            include : ['category','image']
+        })
+        const categories = db.category.findAll({
+            order: [['name']]
+        })
+            Promise.all([products, categories])
+            .then(([products, categories]) => {
+    
+                return res.render('products/product-edit',{
+                    ...products.dataValues,
+                    categories,
+                    errors : errors.mapped()
+                })
+            })
+            .catch(error => console.log(error))
+    
+      }
+    };
+    
