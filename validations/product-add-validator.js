ac2516424809af75cc357041621019d5063ee5d1
gisele ortiz <giseleortizuriel@gmail.com>
2024-02-29 10:05:21 -0300

    corregi product-add-validaciones

--- validations/product-add-validator.js
+++ validations/product-add-validator.js
@@ -1,25 +1,23 @@
-const {check,body} = require('express-validator');
-
-module.exports = [
+const {check, body} = require('express-validator');
+module.exports= [ 
     check('name')
-        .notEmpty().withMessage('El nombre es obligatorio'),
-    check('address')
-        .notEmpty().withMessage('La dirección es requerida'),
-    check('url_map')
-        .isURL().withMessage('Debe ser un URL válida'),
-    check('category')
-        .notEmpty().withMessage('La categoría es requerida'),
+        .notEmpty().withMessage('El nombre es obligatorio').bail(),
+    check('categoryId')
+        .notEmpty().withMessage('Se require especificar una categoría').bail(),
+        check('price')
+        .notEmpty().withMessage('El precio es obligatorio').bail(),
+        check('description')
+        .notEmpty().withMessage('Se requiere una descripción del producto').bail()
+        .isLength({
+            min : 20,
+            max : 500
+        }).withMessage('La descripción debe tener entre 20 y 500 caracteres'),
+
     body('image')
         .custom((value, {req}) => {
             if(!req.files.image){
                 return false
             }
             return true
-        }).withMessage('Se require una imagen'),
-    check('description')
-        .notEmpty().withMessage('La descripción es requerida').bail()
-        .isLength({
-            min: 20,
-            max: 500
-        }).withMessage('La descripción debe tener entre 20 y 500 caracteres'),
-]
+        }).withMessage('Se requiere que tenga al menos una imagen principal')
+               ]
